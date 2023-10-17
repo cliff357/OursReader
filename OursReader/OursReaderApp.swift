a3bdaff4ec69b0d261608a4edba529b9da40958d
Cliff Chan <cliffchan1993@gmail.com>
2023-10-18 07:43:40 +0800

    Initial Commit

---
+++ OursReader/OursReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OursReaderApp.swift
+//  OursReader
+//
+//  Created by Cliff Chan on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct OursReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
